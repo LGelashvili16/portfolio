8b83e7c7b49205fa9f7286da83bfdf33a6de564a
LGelashvili <ladogelashvili16@gmail.com>
2024-09-06 16:41:15 +0400

    fix icons bug

--- constants/index.ts
+++ constants/index.ts
@@ -1,90 +1,90 @@
 export const Socials = [
   {
-    name: 'Github',
-    src: '/icons/Social/github-gray.svg',
-    link: 'https://github.com/LGelashvili16',
+    name: "Github",
+    src: "/icons/Social/github-gray.svg",
+    link: "https://github.com/LGelashvili16",
   },
   {
-    name: 'Discord',
-    src: '/icons/Social/discord-gray.svg',
-    link: 'https://discord.com/users/906907151354724382',
+    name: "Discord",
+    src: "/icons/Social/discord-gray.svg",
+    link: "https://discord.com/users/906907151354724382",
   },
   {
-    name: 'Facebook',
-    src: '/icons/Social/facebook-gray.svg',
-    link: 'https://www.facebook.com/Gelashvili.16/',
+    name: "Facebook",
+    src: "/icons/Social/facebook-gray.svg",
+    link: "https://www.facebook.com/Gelashvili.16/",
   },
   {
-    name: 'Linkedin',
-    src: '/icons/Social/linkedin-gray.svg',
-    link: 'https://www.linkedin.com/in/lado-gelashvili/',
+    name: "Linkedin",
+    src: "/icons/Social/linkedin-gray.svg",
+    link: "https://www.linkedin.com/in/lado-gelashvili/",
   },
 ];
 
 export const FRONTEND_SKILLS = [
   {
-    skill_name: 'HTML',
-    image: '/icons/Frontend/HTML5.svg',
+    skill_name: "HTML",
+    image: "/icons/Frontend/HTML5.svg",
     width: 65,
     height: 65,
   },
   {
-    skill_name: 'CSS',
-    image: '/icons/Frontend/CSS3.svg',
+    skill_name: "CSS",
+    image: "/icons/Frontend/CSS3.svg",
     width: 65,
     height: 65,
   },
   {
-    skill_name: 'SASS',
-    image: '/icons/Frontend/Sass.svg',
+    skill_name: "SASS",
+    image: "/icons/Frontend/Sass.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Tailwind CSS',
-    image: '/icons/Frontend/Tailwind CSS.svg',
+    skill_name: "Tailwind CSS",
+    image: "/icons/Frontend/Tailwind CSS.svg",
     width: 80,
     height: 80,
   },
   {
-    skill_name: 'Bootstrap',
-    image: '/icons/Frontend/Bootstrap.svg',
+    skill_name: "Bootstrap",
+    image: "/icons/Frontend/Bootstrap.svg",
     width: 75,
     height: 75,
   },
   {
-    skill_name: 'JavaScript',
-    image: '/icons/Frontend/JavaScript.svg',
+    skill_name: "JavaScript",
+    image: "/icons/Frontend/JavaScript.svg",
     width: 65,
     height: 65,
   },
   {
-    skill_name: 'TypeScript',
-    image: '/icons/Frontend/TypeScript.svg',
+    skill_name: "TypeScript",
+    image: "/icons/Frontend/TypeScript.svg",
     width: 65,
     height: 65,
   },
   {
-    skill_name: 'React',
-    image: '/icons/Frontend/React.svg',
+    skill_name: "React",
+    image: "/icons/Frontend/React.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Redux',
-    image: '/icons/Frontend/Redux.svg',
+    skill_name: "Redux",
+    image: "/icons/Frontend/Redux.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Tanstack Query',
-    image: '/icons/Frontend/Tanstack.svg',
+    skill_name: "Tanstack Query",
+    image: "/icons/Frontend/Tanstack.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Next.js 14',
-    image: '/icons/Frontend/Nextjs.svg',
+    skill_name: "Next.js 14",
+    image: "/icons/Frontend/Nextjs.svg",
     width: 70,
     height: 70,
   },
@@ -92,32 +92,32 @@ export const FRONTEND_SKILLS = [
 
 export const BACKEND_SKILLS = [
   {
-    skill_name: 'Node.js',
-    image: '/icons/Backend/Nodejs.svg',
+    skill_name: "Node.js",
+    image: "/icons/Backend/Nodejs.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Express.js',
-    image: '/icons/Backend/Express.svg',
+    skill_name: "Express.js",
+    image: "/icons/Backend/Express.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'MongoDB',
-    image: '/icons/Backend/MongoDB.svg',
+    skill_name: "MongoDB",
+    image: "/icons/Backend/MongoDB.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'Firebase',
-    image: '/icons/Backend/Firebase.svg',
+    skill_name: "Firebase",
+    image: "/icons/Backend/Firebase.svg",
     width: 70,
     height: 70,
   },
   {
-    skill_name: 'GraphQL',
-    image: '/icons/Backend/GraphQL.svg',
+    skill_name: "GraphQL",
+    image: "/icons/Backend/GraphQL.svg",
     width: 70,
     height: 70,
   },
@@ -125,20 +125,20 @@ export const BACKEND_SKILLS = [
 
 export const SOFTWARE_TOOLS = [
   {
-    skill_name: 'Git',
-    image: '/icons/Tools/Git.svg',
+    skill_name: "Git",
+    image: "/icons/Tools/Git.svg",
     width: 50,
     height: 50,
   },
   {
-    skill_name: 'Figma',
-    image: '/icons/Tools/Figma.svg',
+    skill_name: "Figma",
+    image: "/icons/Tools/Figma.svg",
     width: 50,
     height: 50,
   },
   {
-    skill_name: 'AdobeXD',
-    image: '/icons/Tools/Adobe_XD.svg',
+    skill_name: "AdobeXD",
+    image: "/icons/Tools/Adobe_XD.svg",
     width: 50,
     height: 50,
   },
@@ -146,92 +146,92 @@ export const SOFTWARE_TOOLS = [
 
 export const FOOTER_CONTACT = [
   {
-    name: 'Github',
-    src: '/icons/social/github-gray.svg',
-    link: 'https://github.com/LGelashvili16',
+    name: "Github",
+    src: "/icons/Social/github-gray.svg",
+    link: "https://github.com/LGelashvili16",
   },
   {
-    name: 'Discord',
-    src: '/icons/Social/discord-gray.svg',
-    link: 'https://discord.com/users/906907151354724382',
+    name: "Discord",
+    src: "/icons/Social/discord-gray.svg",
+    link: "https://discord.com/users/906907151354724382",
   },
   {
-    name: 'Telegram',
-    src: '/icons/Social/telegram-gray.svg',
-    link: 'https://t.me/LGelashvili',
+    name: "Telegram",
+    src: "/icons/Social/telegram-gray.svg",
+    link: "https://t.me/LGelashvili",
   },
   {
-    name: 'Gmail',
-    src: '/icons/Social/gmail-gray.svg',
-    link: 'mailto:ladogelashvili16@gmail.com',
+    name: "Gmail",
+    src: "/icons/Social/gmail-gray.svg",
+    link: "mailto:ladogelashvili16@gmail.com",
   },
 ];
 
 export const FOOTER_SOCIALS = [
   {
-    name: 'Linkedin',
-    src: '/icons/social/linkedin-gray.svg',
-    link: 'https://www.linkedin.com/in/lado-gelashvili/',
+    name: "Linkedin",
+    src: "/icons/Social/linkedin-gray.svg",
+    link: "https://www.linkedin.com/in/lado-gelashvili/",
   },
   {
-    name: 'X',
-    src: '/icons/social/x-gray.svg',
-    link: 'https://x.com/LGelashvili16',
+    name: "X",
+    src: "/icons/Social/x-gray.svg",
+    link: "https://x.com/LGelashvili16",
   },
   {
-    name: 'Facebook',
-    src: '/icons/social/facebook-gray.svg',
-    link: 'https://www.facebook.com/Gelashvili.16/',
+    name: "Facebook",
+    src: "/icons/Social/facebook-gray.svg",
+    link: "https://www.facebook.com/Gelashvili.16/",
   },
   {
-    name: 'Instagram',
-    src: '/icons/social/instagram-gray.svg',
-    link: 'https://www.instagram.com/lgelashvili16/',
+    name: "Instagram",
+    src: "/icons/Social/instagram-gray.svg",
+    link: "https://www.instagram.com/lgelashvili16/",
   },
 ];
 
 export const FOOTER_ABOUT = [
   {
-    title: 'About me',
-    to: '#about-me',
+    title: "About me",
+    to: "#about-me",
   },
   {
-    title: 'Skills',
-    to: '#skills',
+    title: "Skills",
+    to: "#skills",
   },
   {
-    title: 'Projects',
-    to: '#projects',
+    title: "Projects",
+    to: "#projects",
   },
 ];
 
 export const PROJECTS = [
   {
-    src: '/projects/gallery.webp',
-    link: 'https://lgelashvili16.github.io/sweeft',
-    title: 'Photo Gallery',
+    src: "/projects/gallery.webp",
+    link: "https://lgelashvili16.github.io/sweeft",
+    title: "Photo Gallery",
     description:
       "This project is a photo gallery application. Its key components include the implementation of infinite scroll, API caching, and debounce technique. For instance, when users search for 'car,' the data is downloaded. Subsequently, if you search for another term like 'home,' new data is fetched. However, if they search for 'car' again, the data is accessed from the cache instead of being downloaded anew. This approach optimizes data usage and improves performance. Additionally, the application features a history page where all previous search terms are stored.",
   },
   {
-    src: '/projects/online-store.webp',
-    link: 'https://lgelashvili16.github.io/online-store/',
-    title: 'Online Store',
+    src: "/projects/online-store.webp",
+    link: "https://lgelashvili16.github.io/online-store/",
+    title: "Online Store",
     description:
-      'This project is an online store application with various functionalities, including form validation, user creation, user deletion, product management (such as adding products to favorites and cart), and calculating the total sum of products in the cart.',
+      "This project is an online store application with various functionalities, including form validation, user creation, user deletion, product management (such as adding products to favorites and cart), and calculating the total sum of products in the cart.",
   },
   {
-    src: '/projects/book-shop.webp',
-    link: 'https://lgelashvili16.github.io/book-shop/pages/store%20page/index.html',
-    title: 'Book Shop',
+    src: "/projects/book-shop.webp",
+    link: "https://lgelashvili16.github.io/book-shop/pages/store%20page/index.html",
+    title: "Book Shop",
     description:
-      'This project is a bookshop application created solely using DOM manipulations, without any HTML code. It includes functionalities such as adding books to the cart and calculating the total price sum.',
+      "This project is a bookshop application created solely using DOM manipulations, without any HTML code. It includes functionalities such as adding books to the cart and calculating the total price sum.",
   },
   {
-    src: '/projects/online-zoo.webp',
-    link: 'https://lgelashvili16.github.io/online-zoo/pages/main/',
-    title: 'Online Zoo',
+    src: "/projects/online-zoo.webp",
+    link: "https://lgelashvili16.github.io/online-zoo/pages/main/",
+    title: "Online Zoo",
     description:
-      'This project showcases clean HTML, CSS, and JavaScript code. Notably, sliders are implemented without relying on any external libraries.',
+      "This project showcases clean HTML, CSS, and JavaScript code. Notably, sliders are implemented without relying on any external libraries.",
   },
 ];
